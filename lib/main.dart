a6754d182c53664d4b16944673fa0aa77ceb1443
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-07 13:43:57 -0600

    Add Rest time calculation

--- lib/main.dart
+++ lib/main.dart
@@ -26,20 +26,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   num minFirst = 0, minLast = 0, secFirst = 0, secLast = 0;
-  num minutes = 0, fSeconds = 0, lSeconds = 0;
-  String time = "";
+  num minutes = 0, fSeconds = 0, lSeconds = 0, rest = 0;
+  String time = "", restTime = "";
   final _formKey = GlobalKey<FormState>();
 
   num convertToSeconds(time) {
     return time * 60;
   }
 
-  void convertToMinutes(seconds) {
+  String convertToMinutes(seconds) {
     String mins, secs;
     mins = (seconds / 60).floor().toString();
     secs = (seconds % 60).toString().padLeft(2, '0');
 
-    time = "$mins:$secs";
+    return "$mins:$secs";
   }
 
   num totalSeconds(min, sec) {
@@ -52,63 +52,90 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-            child: Form(
-              key: _formKey,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration:
+                          InputDecoration(labelText: "Starting Minutes"),
+                      onChanged: (value) {
+                        minFirst = convertToSeconds(int.parse(value));
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(labelText: "Starting Seconds"),
+                      onChanged: (value) {
+                        secFirst = int.parse(value);
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "Ending Minutes"),
+                      onChanged: (value) {
+                        minLast = convertToSeconds(int.parse(value));
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "Ending Seconds"),
+                      onChanged: (value) {
+                        secLast = int.parse(value);
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "Amount of Rest"),
+                      onChanged: (value) {
+                        rest = int.parse(value);
+                      },
+                      keyboardType: TextInputType.number,
+                    ),
+                    ElevatedButton(
+                      child: const Text('Submit'),
+                      onPressed: () => setState(
+                        () {
+                          fSeconds = minFirst + secFirst;
+                          lSeconds = minLast + secLast;
+
+                          time = convertToMinutes(lSeconds - fSeconds);
+                          restTime =
+                              convertToMinutes((lSeconds - fSeconds) + rest);
+
+                          FocusScope.of(context).unfocus();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
               child: Column(
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "Starting Minutes"),
-                    onChanged: (value) {
-                      minFirst = convertToSeconds(int.parse(value));
-                    },
-                    keyboardType: TextInputType.number,
+                  TextField(
+                    decoration: InputDecoration(labelText: "Start of Rest"),
+                    readOnly: true,
+                    controller: TextEditingController(text: time),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "Starting Seconds"),
-                    onChanged: (value) {
-                      secFirst = int.parse(value);
-                    },
-                    keyboardType: TextInputType.number,
+                  TextField(
+                    decoration: InputDecoration(labelText: "End of Rest"),
+                    readOnly: true,
+                    controller: TextEditingController(text: restTime),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "Ending Minutes"),
-                    onChanged: (value) {
-                      minLast = convertToSeconds(int.parse(value));
-                    },
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: "Ending Seconds"),
-                    onChanged: (value) {
-                      secLast = int.parse(value);
-                    },
-                    keyboardType: TextInputType.number,
-                  ),
-                  ElevatedButton(
-                    child: const Text('Submit'),
-                    onPressed: () => setState(
-                      () {
-                        fSeconds = minFirst + secFirst;
-                        lSeconds = minLast + secLast;
-
-                        convertToMinutes(lSeconds - fSeconds);
-                      },
-                    ),
-                  ),
-                  if (time != "")
-                    Text(
-                      "Time: $time",
-                      style: TextStyle(fontSize: 30),
-                    ),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
